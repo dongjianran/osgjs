d10778154c678975aeb8dde809fefc7c94731602
Stéphane Ginier <stephane.ginier@gmail.com>
2014-01-07 15:03:32 +0100

    [#63365168] Adds picking example

---
+++ examples/picking/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function loadUrl( url, viewer, node ) {
+    osg.log( 'loading ' + url );
+    var req = new XMLHttpRequest();
+    req.open( 'GET', url, true );
+    req.onload = function ( aEvt ) {
+        loadModel( JSON.parse( req.responseText ), viewer, node );
+        osg.log( 'success ' + url );
+    };
+    req.onerror = function ( aEvt ) {
+        osg.log( 'error ' + url );
+    };
+    req.send( null );
+};
+
+function loadModel( data, viewer, node ) {
+    osgDB.Promise.when( osgDB.parseSceneGraph( data ) ).then( function ( child ) {
+        node.addChild( child );
+        viewer.getManipulator().computeHomePosition();
+    } );
+};
+
+function createScene( viewer ) {
+    var canvas = document.getElementById( '3DView' );
+
+    var root = new osg.Node();
+
+    loadModel( getOgre(), viewer, root );
+    // loadModel( getPokerScene(), viewer, root );
+
+    return root;
+};
+
+window.addEventListener( 'load',
+    function () {
+        OSG.globalify();
+
+        var canvas = document.getElementById( '3DView' );
+        canvas.style.width = canvas.width = window.innerWidth;
+        canvas.style.height = canvas.height = window.innerHeight;
+
+        var viewer = new osgViewer.Viewer( canvas );
+        viewer.init();
+        viewer.setSceneData( createScene( viewer ) );
+        viewer.setupManipulator();
+        viewer.run();
+
+        canvas.addEventListener( 'mousedown', function ( ev ) {
+            console.time('t');
+            var hits = viewer.computeIntersections( ev.clientX, canvas.height - ev.clientY );
+            console.timeEnd('t');
+            hits.sort( function ( a, b ) {
+                return a.ratio - b.ratio;
+            } );
+        }, true );
+    }, true );
